3eec279ed0b5f366c9c84e94e7c999f2f5bc46f6
DESKTOP-82QMF48\user <test@test.com>
2024-07-15 18:53:04 +0900

    memo_회원가입, 아이디 중복확인,로그인,로그아웃 완료_24-07-15

--- src/main/java/com/memo/user/UserRestController.java
+++ src/main/java/com/memo/user/UserRestController.java
@@ -13,12 +13,15 @@ import com.memo.common.EncryptUtils;
 import com.memo.user.bo.UserBO;
 import com.memo.user.entity.UserEntity;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @RequestMapping("/user")
 @RestController
 public class UserRestController {
-	
+
 	@Autowired
-	UserBO userBO;
+	private UserBO userBO;
 	
 	/**
 	 * 아이디 중복확인 API
@@ -27,7 +30,7 @@ public class UserRestController {
 	 */
 	@RequestMapping("/is-duplicated-id")
 	public Map<String, Object> isDuplicatedId(
-			@RequestParam("loginId") String loginId ) {
+			@RequestParam("loginId") String loginId) {
 		
 		// db 조회
 		UserEntity user = userBO.getUserEntityByLoginId(loginId);
@@ -36,21 +39,30 @@ public class UserRestController {
 		Map<String, Object> result = new HashMap<>();
 		result.put("code", 200);
 		if (user != null) {
-			result.put("is-duplicated-id", true);
+			result.put("is_duplicated_id", true);
 		} else {
-			result.put("is-duplicated-id", false);			
+			result.put("is_duplicated_id", false);
 		}
 		return result;
 	}
-
+	
+	/**
+	 * 회원가입 API
+	 * @param loginId
+	 * @param password
+	 * @param name
+	 * @param email
+	 * @return
+	 */
 	@PostMapping("/sign-up")
 	public Map<String, Object> signUp(
-			@RequestParam("loginId")String loginId,
-			@RequestParam("password")String password,
-			@RequestParam("name")String name,
-			@RequestParam("email")String email) {
+			@RequestParam("loginId") String loginId,
+			@RequestParam("password") String password,
+			@RequestParam("name") String name,
+			@RequestParam("email") String email) {
 		
-		// password md5 알고리즘 => hashing
+		// password md5 알고리즘 => hashing   
+		// aaaa => 74b8733745420d4d33f80c4663dc5e5
 		// aaaa => 74b8733745420d4d33f80c4663dc5e5
 		String hashedPassword = EncryptUtils.md5(password);
 		
@@ -61,11 +73,49 @@ public class UserRestController {
 		Map<String, Object> result = new HashMap<>();
 		if (user != null) {
 			result.put("code", 200);
-			result.put("result", "성공");			
+			result.put("result", "성공");
 		} else {
 			result.put("code", 500);
-			result.put("error_message", "회원가입에 실패했습니다.");						
+			result.put("error_message", "회원가입에 실패했습니다.");
 		}
+		return result;
+	}
+	
+	/**
+	 * 로그인 API
+	 * @param loginId
+	 * @param password
+	 * @param request
+	 * @return
+	 */
+	@PostMapping("/sign-in")
+	public Map<String, Object> signIn(
+			@RequestParam("loginId") String loginId,
+			@RequestParam("password") String password,
+			HttpServletRequest request) {
+		
+		// password 해싱
+		String hashedPassword = EncryptUtils.md5(password);
+		
+		// DB 조회 - loginId, 해싱된 비밀번호 => UserEntity
+		UserEntity user = userBO.getUserEntityByLoginIdPassword(loginId, hashedPassword);
+		
+		// 로그인 처리 및 응답값
+		Map<String, Object> result = new HashMap<>();
+		if (user != null) { // 성공
+			// 세션에 사용자 정보를 담는다.(사용자 각각 마다)
+			HttpSession session = request.getSession();
+			session.setAttribute("userId", user.getId());
+			session.setAttribute("userLoginId", user.getLoginId());
+			session.setAttribute("userName", user.getName());
+			
+			result.put("code", 200);
+			result.put("result", "성공");
+		} else { // 실패
+			result.put("code", 403);
+			result.put("error_message", "존재하지 않는 사용자입니다.");
+		}
+		
 		return result;
 	}
 }
